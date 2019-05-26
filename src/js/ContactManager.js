5e0cd7336644308be34e916ac635d38dfb1b717a
JFuentes4 <otro64@hotmail.com>
2019-05-26 15:29:08 -0500

    persistencia en local storage

--- src/js/ContactManager.js
+++ src/js/ContactManager.js
@@ -3,25 +3,45 @@ import GraphicManager from './GraphicManager.js'
 
 class ContactManager {
   constructor () {
+    this._dataContacts = [];
     this._contacts = [];
     this._graphicManager = new GraphicManager();
+    this._initializeTable();
+  }
+
+  _initializeTable  = () => {
+    let contacts = JSON.parse(localStorage.getItem('contactsList'));
+    if (contacts !== null) {
+      console.log(contacts)
+      contacts.forEach((contact, index) => {
+        console.log(contact._name)
+        this._setContactToArrayAndLS(contact);
+      });
+      this._graphicManager.showOnTable(this._contacts);
+    }
   }
 
   addContact = (dataContact) => {
     if (this._ifDontExist(dataContact.telephone)) {
-      let contact = new Contact(dataContact);
-      this._contacts.push(contact);
-      localStorage.setItem('contactsList', JSON.stringify(this._contacts));
+      this._setContactToArrayAndLS(dataContact);
       this._graphicManager.showOnTable(this._contacts);
-      console.log(this._contacts)
     } else {
-      alert('este telefono ya esta actualemnet registrado');
+      alert('este telefono ya esta actualmente registrado');
     }
+  }
+
+  _setContactToArrayAndLS = (dataContact) => {
+    let contact = new Contact(dataContact);
+    this._contacts.push(contact);
+    this._dataContacts.push(dataContact);
+    localStorage.setItem('contactsList', JSON.stringify(this._dataContacts));
+    console.log(this._contacts)
   }
 
   _deleteContact = (index) => {
     this._contacts.splice(index, 1);
-    localStorage.setItem('contactsList', JSON.stringify(this._contacts));
+    this._dataContacts.splice(index, 1);
+    localStorage.setItem('contactsList', JSON.stringify(this._dataContacts));
   }
 
   _ifDontExist = (telephone) => {
